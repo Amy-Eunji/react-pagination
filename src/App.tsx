e4a712f1a91769d01b3eba24f93436a51facf5d2
Amy-Eunji <104086211+Amy-Eunji@users.noreply.github.com>
2023-08-24 15:30:12 +0900

    pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { QueryClientProvider, QueryClient } from "react-query";
 import Main from "./components/pages/Main";
 
 const App = () => {
-  return <Main />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Main />;
+    </QueryClientProvider>
+  );
 };
 
 export default App;
--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,41 +1,60 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useQuery } from "react-query";
+
 import Pagination from "../templates/Pagination";
+import axios from "axios";
 
 const Main = () => {
-  const [posts, setPosts] = useState([]);
+  // const [post, setPost] = useState([]);
   const [totalPosts, setTotalPosts] = useState(10);
   const [page, setPage] = useState(1);
   const offset = (page - 1) * totalPosts;
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, []);
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then((res) => res.json())
+  //     .then((data) => setPosts(data));
+  // }, []);
+
+  const fetchPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+    return data;
+  };
+
+  const { data: posts, isLoading, error } = useQuery("posts", fetchPosts);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error</div>;
+  }
 
   return (
     <Container>
       <Title> Pagination 📎 </Title>
-      <SortWrap>
-        <select
-          value={totalPosts}
-          onChange={({ target: { value } }) => setTotalPosts(Number(value))}
-        >
-          <option value="10">10개씩 보기</option>
-          <option value="20">20개씩 보기</option>
-          <option value="30">30개씩 보기</option>
-        </select>
-      </SortWrap>
+      <select
+        value={totalPosts}
+        onChange={({ target: { value } }) => setTotalPosts(Number(value))}
+      >
+        <option value="10">10개씩 보기</option>
+        <option value="20">20개씩 보기</option>
+        <option value="30">30개씩 보기</option>
+      </select>
       <div>
-        {posts.slice(offset, offset + totalPosts).map(({ id, title, body }) => (
-          <div key={id}>
-            <h3>
-              {id}. {title}
-            </h3>
-            <p>{body}</p>
-          </div>
-        ))}
+        {posts
+          .slice(offset, offset + totalPosts)
+          .map(({ id, title, body }: any) => (
+            <div key={id}>
+              <h3>
+                {id}. {title}
+              </h3>
+              <p>{body}</p>
+            </div>
+          ))}
       </div>
       <Pagination
         total={posts.length}
@@ -52,20 +71,17 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`;
-
-const Title = styled.h2`
-  font-size: 50px;
-`;
-
-const SortWrap = styled.div`
-  margin: 20px auto;
   select {
+    margin: 20px auto;
     width: 120px;
     height: 30px;
     padding: 0 7px 0;
     border-radius: 5px;
   }
+`;
+
+const Title = styled.h2`
+  font-size: 50px;
 `;
 
 export default Main;
